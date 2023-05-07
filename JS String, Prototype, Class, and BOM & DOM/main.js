f4f60bca6a1fdbffd8c9e31350b90f3b8ac7ed03
zulfuqar1 <zulfugarga@code.edu.az>
2023-05-07 22:34:44 +0400

    finished

---
+++ JS String, Prototype, Class, and BOM & DOM/main.js
@@ -0,0 +1,27 @@
+// (1)
+//function string_to_array(stringToArray) 
+//{
+//    var convert  = stringToArray.split(" ");
+//    return convert ;
+//}
+//  console.log( string_to_array("Robin Singh"));
+
+
+//function truncate_string(str, num_chars) 
+//{
+//    var extracted_string = str.slice(0, num_chars);
+//    return extracted_string;
+//}
+//  console.log(truncate_string("Robin Singh",4));
+
+
+
+
+//function protect_email(email) 
+//{
+//    var part = email.split("@");
+//    var hidden_chars = part[0].length - 3;
+//    var hidden_email = part[0].substr(0, 3) + "*".repeat(hidden_chars) + "@" + part[1];
+//    return hidden_email;
+//}
+//console.log(protect_email("zulfuqareliyev520@gmail.com"));
